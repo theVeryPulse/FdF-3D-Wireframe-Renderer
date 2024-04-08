6b226a5ce71502431b26157babe3fd6de7f68c0c
Philip <macpilot23@outlook.com>
2024-04-08 21:50:29 +0100

    remove old files/funcs

--- src/read_file.c
+++ src/read_file.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 19:35:00 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/08 21:32:00 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/08 21:44:36 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,41 +53,6 @@ char	*read_file(const char *file)
 	ft_lstclear(&list, free);
 	return (all);
 }
-
-#if 0
-/**
- * @brief Converts a hexadecimal string to an integer.
- *
- * @param str The hexadecimal string to convert.
- * @return The integer value of the hexadecimal string.
- */
-int	hex_atoi(const char *str)
-{
-	size_t				i;
-	int					n;
-	char				*copy;
-	static const char	*lower = "0123456789abcdef";
-
-	copy = ft_strndup(str, 12);
-	n = 0;
-	i = 2;
-	while (copy[i])
-	{
-		if (ft_isupper(copy[i]))
-			copy[i] = ft_tolower(copy[i]);
-		i++;
-	}
-	i = 2;
-	while (ft_strchr(lower, copy[i]))
-	{
-		n *= 16;
-		n += (int)(ft_strchr(lower, copy[i]) - lower);
-		i++;
-	}
-	free(copy);
-	return (n);
-}
-#endif
 
 /**
  * @brief Converts a linked list of strings into a single string.
